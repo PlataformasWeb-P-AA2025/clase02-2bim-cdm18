44030349549c3a53cbdc0b713f9f277f86ca9caa
Carlos <cdmejia4@utpl.edu.ec>
2025-06-11 08:30:44 -0500

    desarrollo - django class Estudiante

--- ejemplo00/app01/views.py
+++ ejemplo00/app01/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from administrativo.models import Estudiante
+
 
 # Create your views here.
+
+def estudiantes_list(request):
+    estudiantes = Estudiante.objects.all()
+    return render(request, 'templates/estudiantes_list.html', {'estudiantes': estudiantes})
